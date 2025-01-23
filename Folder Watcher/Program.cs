27e815d036b8ad9502548ac2dee38db179f84577
Hesam <hesoyam.h.0913@gmail.com>
2025-01-23 17:02:10 +0330

    Add project files.

---
+++ Folder Watcher/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Globalization;
+using System.Text.Json;
+
+namespace FolderWatcher
+{
+    class Program
+    {
+        static void Main()
+        {
+             string folderPath = File.ReadAllText("appsettings.txt");
+
+
+            string logFilePath = @$"{folderPath}\LogChanges.txt";
+
+            FileSystemWatcher watcher = new(folderPath)
+            {
+                IncludeSubdirectories = true,
+                EnableRaisingEvents = true
+            };
+            watcher.Created += (sender, e) => LogChange("Created", e.FullPath, logFilePath);
+            watcher.Deleted += (sender, e) => LogChange("Deleted", e.FullPath, logFilePath);
+
+            Console.WriteLine("Press ENTER to exit.");
+
+            // still runnig to press key
+            while (true)
+            {
+                string userInput = Console.ReadLine() ?? "";
+                if (string.IsNullOrEmpty(userInput))
+                    break;
+            }
+        }
+
+        private static void LogChange(string action, string path, string logFilePath)
+        {
+            string[] parts = path.Split('\\');
+            string secondLast = parts[parts.Length - 2];
+            string last = parts[parts.Length - 1];
+
+            string logEntry = $"{action}==> '{secondLast}'  {last}\n";
+            File.AppendAllText(logFilePath, logEntry);
+            Console.WriteLine(logEntry);
+        }
+    }
+
+
+}
